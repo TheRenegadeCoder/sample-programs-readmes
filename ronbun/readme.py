0df623fc9628cc8fdc2c778da94db558a7789ef8
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-08 19:53:46 -0400

    Ported the Sample Programs READMEs Code to New Repo (#1)
    * Switched out README file
    
    * Came up with name
    
    * Created a main function
    
    * Removed extra junk
    
    * Updated some links

---
+++ ronbun/readme.py
@@ -0,0 +1,157 @@
+import argparse
+import logging
+
+from snakemd import Document, InlineText, MDList, Paragraph
+from subete import LanguageCollection, Repo
+
+
+def main():
+    args = _get_args()
+    numeric_level = getattr(logging, args[1].upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f'Invalid log level: {args[1]}')
+    logging.basicConfig(level=numeric_level)
+    repo = Repo(source_dir=args[0])
+    readme_catalog = ReadMeCatalog(repo)
+    for language, page in readme_catalog.pages.items():
+        page.output_page(f"{args[0]}/{language[0]}/{language}")
+
+
+def _get_args() -> tuple:
+    """
+    A helper function which gets the log level from 
+    the command line. Set as warning from default. 
+    :return: the log level provided by the user
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument(
+        "-log",
+        "--log",
+        default="warning",
+        help=(
+            "Provide logging level. "
+            "Example --log debug', default='warning'"
+        ),
+    )
+    options = parser.parse_args()
+    return options.path, options.log
+
+
+def _get_intro_text(language: LanguageCollection) -> Paragraph:
+    paragraph = Paragraph([f"Welcome to Sample Programs in {language}! "])
+    text = InlineText("here.", url=language.lang_docs_url())
+    if text.verify_url():
+        paragraph.add(f"To find documentation related to the {language} code in this repo, look ")
+        paragraph.add(text)
+    return paragraph
+
+
+def _get_sample_programs_text() -> str:
+    return """
+    Below, you'll find a list of code snippets in this collection.
+    Code snippets preceded by :warning: link to a GitHub 
+    issue query featuring a possible article request issue. If an article request issue 
+    doesn't exist, we encourage you to create one. Meanwhile, code snippets preceded 
+    by :white_check_mark: link to an existing article which provides further documentation.
+    """
+
+
+def _generate_program_list(language: LanguageCollection) -> MDList:
+    """
+    A helper function which generates a list of programs for the README.
+    :param language: a language collection
+    :return: a list of sample programs list items
+    """
+    list_items = list()
+    for program in language.sample_programs().values():
+        program_name = f"{program}"
+        program_line = Paragraph([f":white_check_mark: {program_name} [Requirements]"]) \
+            .insert_link(program_name, program.documentation_url()) \
+            .insert_link("Requirements", program.requirements_url())
+        if not program_line.verify_urls()[program.documentation_url()]:
+            program_line.replace(":white_check_mark:", ":warning:") \
+                .replace_link(program.documentation_url(), program.article_issue_query_url())
+        list_items.append(program_line)
+    return MDList(list_items)
+
+
+def _generate_credit() -> Paragraph:
+    p = Paragraph([
+        """
+        This page was generated automatically by the Sample Programs READMEs tool. 
+        Find out how to support this project on Github.
+        """
+    ])
+    p.insert_link("this project", "https://github.com/TheRenegadeCoder/sample-programs-readmes")
+    return p
+
+
+class ReadMeCatalog:
+    """
+    An representation of the collection of READMEs in the Sample Programs repo.
+    """
+
+    def __init__(self, repo: Repo):
+        """
+        Constructs an instance of a ReadMeCatalog.
+        :param repo: a repository instance
+        """
+        self.repo: Repo = repo
+        self.pages: dict[str, Document] = dict()
+        self._build_readmes()
+
+    def _build_readme(self, language: LanguageCollection) -> None:
+        """
+        Creates a README page from a language collection.
+        :param language: a programming language collection (e.g., Python)
+        :return: None
+        """
+        page = Document("README")
+
+        # Introduction
+        page.add_header(f"Sample Programs in {language}")
+        page.add_element(_get_intro_text(language))
+
+        # Sample Programs List
+        page.add_header("Sample Programs List", level=2)
+        page.add_paragraph(_get_sample_programs_text())
+        page.add_element(_generate_program_list(language))
+
+        # Testing
+        page.add_header("Testing", level=2)
+        test_data = language.testinfo()
+        if not test_data:
+            page.add_paragraph(
+                """
+                This language currently does not feature testing. If you'd like to help in the efforts to test all of 
+                the code in this repo, consider creating a testinfo.yml file with the following information:
+                """
+            )
+            page.add_code("folder:\n  extension:\n  naming:\n\ncontainer:\n  image:\n  tag:\n  cmd:", lang="yml")
+        else:
+            page.add_paragraph(
+                f"The following list shares details about what we're using to test all Sample Programs in {language}."
+            )
+            page.add_unordered_list([
+                f"Docker Image: {test_data['container']['image']}",
+                f"Docker Tag: {test_data['container']['tag']}"
+            ])
+        glotter = page.add_paragraph("See the Glotter project for more information on how to create a testinfo file.")
+        glotter.insert_link("Glotter project", "https://github.com/auroq/glotter")
+        page.add_horizontal_rule()
+        page.add_element(_generate_credit())
+
+        self.pages[language.pathlike_name()] = page
+
+    def _build_readmes(self) -> None:
+        """
+        Generates all READMEs for the repo.
+        :return: None
+        """
+        for _, language in self.repo.language_collections().items():
+            self._build_readme(language)
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -4,23 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="yomu",
+    name="ronbun",
     version="0.1.0",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
-    description="Generates the README for the 'How to Python Code' repo",
+    description="The Sample Programs README Automation Tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TheRenegadeCoder/how-to-python-readme",
+    url="https://github.com/TheRenegadeCoder/sample-programs-readmes",
     packages=setuptools.find_packages(),
     install_requires=[
-        "feedparser>=6",
-        "beautifulsoup4>=4",
-        "SnakeMD>=0"
+        "SnakeMD>=0",
+        "subete>=0"
     ],
     entry_points={
         "console_scripts": [
-            'yomu = yomu.readme:main'
+            'ronbun = ronbun.readme:main'
         ],
     },
     classifiers=(
