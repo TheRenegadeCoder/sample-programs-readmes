b21437816ace0f7fc6ac2004d4174993dd5ed7ff
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-05-10 22:33:49 -0400

    Upgraded package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ronbun",
-    version="0.4.2",
+    version="0.4.3",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
     description="The Sample Programs README Automation Tool",
