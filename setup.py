987681856c3991d0d11d81ad91ab26889810a33a
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-04-08 12:45:31 -0500

    Need to bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ronbun",
-    version="0.7.1",
+    version="0.7.2",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
     description="The Sample Programs README Automation Tool",
